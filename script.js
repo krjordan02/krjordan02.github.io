3ed0f6e36212f03145457267704fbacde528676f
krjordan02 <keeganrjordan@gmail.com>
2023-12-15 14:59:17 -0700

    Cards commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const addFooter = () => {
     let container = document.getElementsByClassName('footer-container');
-    console.log(container)
 
     let copyright = document.createElement('div');
     copyright.innerHTML = '<p class="footer-text">© 2023 Keegan Jordan</p>';
@@ -17,3 +16,30 @@ const addFooter = () => {
         element.appendChild(socials);
     }
 }
+
+const addCard = (title, img, info, link) => {
+    let container = document.getElementsByClassName('project-cards-sub-container');
+
+    let card = document.createElement('div');
+    card.innerHTML = '<p class="card-title">' + title + '</p>' +
+    '<img src="./' + img + '" alt="Project Image" class="card-img">' +
+    '<p class="card-info">' + info + '</p>' +
+    '<div class="explore-button-card-container">' +
+        '<a href="#' + link + '" class="explore-button">Explore<i class="bi bi-arrow-right" id="explore-button-arrow" ></i></a>' +
+    '</div>';
+    card.classList = 'card';
+
+    for(let element of container){
+        element.appendChild(card);
+    }
+
+}
+
+/*
+    '<img src="./readMeImgBus.jpeg" alt="Project Image" class="card-img">'
+    '<p class="card-info">A live bus tracker that displays all of the current Massachusetts Bay Transportation Authority busses on a map.</p>'
+    '<div class="explore-button-card-container">'
+        '<a href="./projects.html" class="explore-button">Explore<i class="bi bi-arrow-right" id="explore-button-arrow" ></i></a>'
+    '</div>'
+
+*/
